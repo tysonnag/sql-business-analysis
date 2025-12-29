69ad682993c7c30629885c078b3bfa49e9da3b2d
NagaMallikarjun-DataAnalyst <96073090+tysonnag@users.noreply.github.com>
2025-12-29 23:15:53 +0530

    Update window_functions.sql

--- queries/queries/window_functions.sql
+++ queries/queries/window_functions.sql
@@ -1,7 +1,1 @@
--- Ranking sites by average OTD
-SELECT 
-    site_code,
-    AVG(otd_minutes) AS avg_otd,
-    RANK() OVER (ORDER BY AVG(otd_minutes)) AS performance_rank
-FROM orders
-GROUP BY site_code;
+
